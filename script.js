b50c7464b339d704eadf439b8a98ad8e364fc000
lezzka mods <lolmem601@gmail.com>
2024-03-22 23:59:33 +0200

    ideterminate slider

--- script.js
+++ script.js
@@ -5,3 +5,41 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+// slider
+const slides = document.querySelectorAll(".slide")
+var counter = 0;
+var counterMax = 5;
+// console.log(slides)
+slides.forEach(
+    (slide,index) => {
+        slide.style.left = `${index * 47}%`
+    }
+)
+
+const goPrev = () => {
+    if(counter > 1){
+    counter--
+    slideImage()  
+    } else {
+        counter = 6
+        SlideImage()
+    }
+}
+
+const goNext = () => {
+    if(counter < counterMax){
+        counter++
+        slideImage()
+    } else {
+        counter = -1
+        SlideImage()
+    }
+}
+
+const slideImage =() => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 147}%)`
+        }
+    )
+}
